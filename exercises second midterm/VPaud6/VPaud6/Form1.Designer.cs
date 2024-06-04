649a5a94cbb9918d2e9228a5b55a94bbc067047d
Mila Stamatovska <152219434+mila-stam@users.noreply.github.com>
2024-06-05 00:33:14 +0200

    add second midterm exercises

---
+++ exercises second midterm/VPaud6/VPaud6/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace VPaud6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbWordState = new System.Windows.Forms.TextBox();
+            this.tbTimer = new System.Windows.Forms.TextBox();
+            this.tbLetter = new System.Windows.Forms.TextBox();
+            this.btnTryLetter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbGuessedLetters = new System.Windows.Forms.TextBox();
+            this.pbTimer = new System.Windows.Forms.ProgressBar();
+            this.pbWrongTries = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbWordState
+            // 
+            this.tbWordState.Location = new System.Drawing.Point(45, 102);
+            this.tbWordState.Name = "tbWordState";
+            this.tbWordState.ReadOnly = true;
+            this.tbWordState.Size = new System.Drawing.Size(520, 22);
+            this.tbWordState.TabIndex = 0;
+            // 
+            // tbTimer
+            // 
+            this.tbTimer.Location = new System.Drawing.Point(524, 36);
+            this.tbTimer.Name = "tbTimer";
+            this.tbTimer.ReadOnly = true;
+            this.tbTimer.Size = new System.Drawing.Size(77, 22);
+            this.tbTimer.TabIndex = 1;
+            // 
+            // tbLetter
+            // 
+            this.tbLetter.Location = new System.Drawing.Point(52, 177);
+            this.tbLetter.Name = "tbLetter";
+            this.tbLetter.Size = new System.Drawing.Size(109, 22);
+            this.tbLetter.TabIndex = 2;
+            // 
+            // btnTryLetter
+            // 
+            this.btnTryLetter.Location = new System.Drawing.Point(186, 177);
+            this.btnTryLetter.Name = "btnTryLetter";
+            this.btnTryLetter.Size = new System.Drawing.Size(75, 23);
+            this.btnTryLetter.TabIndex = 3;
+            this.btnTryLetter.Text = "OK";
+            this.btnTryLetter.UseVisualStyleBackColor = true;
+            this.btnTryLetter.Click += new System.EventHandler(this.btnTryLetter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // tbGuessedLetters
+            // 
+            this.tbGuessedLetters.Location = new System.Drawing.Point(45, 273);
+            this.tbGuessedLetters.Name = "tbGuessedLetters";
+            this.tbGuessedLetters.ReadOnly = true;
+            this.tbGuessedLetters.Size = new System.Drawing.Size(553, 22);
+            this.tbGuessedLetters.TabIndex = 5;
+            // 
+            // pbTimer
+            // 
+            this.pbTimer.Location = new System.Drawing.Point(45, 388);
+            this.pbTimer.Name = "pbTimer";
+            this.pbTimer.Size = new System.Drawing.Size(553, 39);
+            this.pbTimer.TabIndex = 6;
+            // 
+            // pbWrongTries
+            // 
+            this.pbWrongTries.Location = new System.Drawing.Point(45, 326);
+            this.pbWrongTries.Name = "pbWrongTries";
+            this.pbWrongTries.Size = new System.Drawing.Size(553, 39);
+            this.pbWrongTries.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 477);
+            this.Controls.Add(this.pbWrongTries);
+            this.Controls.Add(this.pbTimer);
+            this.Controls.Add(this.tbGuessedLetters);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTryLetter);
+            this.Controls.Add(this.tbLetter);
+            this.Controls.Add(this.tbTimer);
+            this.Controls.Add(this.tbWordState);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWordState;
+        private System.Windows.Forms.TextBox tbTimer;
+        private System.Windows.Forms.TextBox tbLetter;
+        private System.Windows.Forms.Button btnTryLetter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbGuessedLetters;
+        private System.Windows.Forms.ProgressBar pbTimer;
+        private System.Windows.Forms.ProgressBar pbWrongTries;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ exercises second midterm/VPaud6/VPaud6/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPaud6
+{
+    public partial class Form1 : Form
+    {
+        public RandomPicker randomPicker {  get; set; }
+        public HangmanWord hangmanWord { get; set; }
+        int timeLeft;
+            
+        public Form1()
+        {
+            InitializeComponent();
+            randomPicker = new RandomPicker();
+            init(); 
+        }
+        private void init()
+        {
+            hangmanWord = new HangmanWord(randomPicker.pick());
+            tbTimer.Text = "02:00";
+            updateTextBoxes();
+            pbWrongTries.Maximum = HangmanWord.MAX_RETRIES;
+            pbWrongTries.Value = 0;
+            timeLeft = 120;
+            timer1.Start();
+            pbTimer.Maximum = 120;
+            pbTimer.Value = 120;
+            tbLetter.Text = "";
+        }
+        private void updateTextBoxes()
+        {
+            tbWordState.Text = hangmanWord.GetMaskedWord();
+            tbGuessedLetters.Text = hangmanWord.getMaskedGuessedLetters();
+
+        }
+        private void ShowMessage(string description)
+        {
+            DialogResult dr = MessageBox.Show(description, "Game Over", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                //nova igra
+                 init();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+        private void btnTryLetter_Click(object sender, EventArgs e)
+        {
+              if(!string.IsNullOrEmpty(tbLetter.Text))
+            {
+                char letter = tbLetter.Text[0];
+                hangmanWord.GuessLetter(letter);
+                updateTextBoxes();
+                pbWrongTries.Value = hangmanWord.WrongCount;
+                if (hangmanWord.Win())
+                {
+                    timer1.Stop();
+                    ShowMessage("Congrats, you won! Do you want a new game?");
+                }
+                else if(hangmanWord.Lose())
+                {
+                    timer1.Stop();
+                    ShowMessage("Sorry, you lost! Do you want a new game?");
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft -= 1;
+            tbTimer.Text = String.Format("{0}:{1}", timeLeft / 60, timeLeft % 60);
+            pbTimer.Value = timeLeft;   
+            if(timeLeft <= 0)
+            {
+                //timer1.Stop();
+                ShowMessage("Sorry, time over ! Do you want a new game?");
+            }
+        }
+    }
+}
